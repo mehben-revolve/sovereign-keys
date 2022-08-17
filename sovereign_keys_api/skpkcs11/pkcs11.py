9c04dca44a74706e6e4a286022c891e550de1290
Alexandre Knecht <alexandre.knecht@revolve.team>
2022-08-17 15:35:38 +0200

    Toggling off costly resources

--- sovereign_keys_api/skpkcs11/pkcs11.py
+++ sovereign_keys_api/skpkcs11/pkcs11.py
@@ -4,17 +4,17 @@ from ctypes import CDLL
 from ctypes import byref
 from ctypes import c_ulong
 
-from .constants import UserType, Error, Mechanism, ObjectType, Source, MGF, KeyType, PrfDataType
+from skpkcs11.constants import UserType, Error, Mechanism, ObjectType, Source, MGF, KeyType, PrfDataType
 import skpkcs11.vendordefinedconstants.cloudhsm as cloudhsm_constants
 import skpkcs11.vendordefinedconstants.proteccio as proteccio_constants
-from .types import SessionFlag, Attribute
-from .types import CK_RSA_PKCS_OAEP_PARAMS, CK_GCM_PARAMS, CK_KEY_DERIVATION_STRING_DATA
-from .types import CK_SESSION_INFO, CK_ATTRIBUTE, CK_MECHANISM, CK_MECHANISM_INFO
-from .types import CK_SP800_108_COUNTER_FORMAT, CK_SP800_108_DKM_LENGTH_FORMAT
-from .types import CK_PRF_DATA_PARAM, CK_SP800_108_KDF_PARAMS
-from .types import CK_C_INITIALIZE_ARGS
+from skpkcs11.types import SessionFlag, Attribute
+from skpkcs11.types import CK_RSA_PKCS_OAEP_PARAMS, CK_GCM_PARAMS, CK_KEY_DERIVATION_STRING_DATA
+from skpkcs11.types import CK_SESSION_INFO, CK_ATTRIBUTE, CK_MECHANISM, CK_MECHANISM_INFO
+from skpkcs11.types import CK_SP800_108_COUNTER_FORMAT, CK_SP800_108_DKM_LENGTH_FORMAT
+from skpkcs11.types import CK_PRF_DATA_PARAM, CK_SP800_108_KDF_PARAMS
+from skpkcs11.types import CK_C_INITIALIZE_ARGS
 
-from .utils import get_void_pointer_and_len, get_value_from_void_pointer_and_len, get_buffer
+from skpkcs11.utils import get_void_pointer_and_len, get_value_from_void_pointer_and_len, get_buffer
 
 from threading import Lock
 
@@ -27,7 +27,7 @@ class HSMFamily(Enum):
 class PKCS11Lib:
     __known_lib = {}
     __factory_lock = Lock()
-
+    
     def __init__(self, pkcs11_lib_path, hsm_family):
         # Load Proteccio PKCS11 lib
         self.__lib = CDLL(pkcs11_lib_path)
@@ -36,7 +36,7 @@ class PKCS11Lib:
         self.__init_count = 0
         self.__initialized = False
         self.__remote_op_lock = Lock()
-
+    
     def __initialize(self):
         logger.debug('Entering PKCS11Lib.__initialize')
         # Initialize
@@ -49,7 +49,7 @@ class PKCS11Lib:
             self.__lib.C_Finalize(None)
             raise Exception(f'C_Initialize failed with error {Error(RV).name}')
         logger.debug('Exiting PKCS11Lib.__initialize')
-
+    
     def __finalize(self):
         logger.debug('Entering PKCS11Lib.__finalize')
         # Finalize
@@ -57,42 +57,42 @@ class PKCS11Lib:
         if RV != 0:
             raise Exception(f'C_Finalize failed with error {Error(RV).name}')
         logger.debug('Exiting PKCS11Lib.__finalize')
-
+    
     def _enter(self):
         with self.__init_lock:
             if self.__init_count == 0:
                 self.__initialize()
             self.__init_count += 1
-
+    
     def __enter__(self):
         self._enter()
         return self
-
+    
     def _exit(self):
         with self.__init_lock:
             self.__init_count -= 1
             if self.__init_count <= 0:
                 self.__finalize()
-
+    
     def __exit__(self, type_, value, traceback):
         self._exit()
-
+    
     @property
     def remote_op_lock(self):
         return self.__remote_op_lock
-
+    
     @property
     def lib(self):
         return self.__lib
-
+    
     @property
     def hsm_family(self):
         return self.__hsm_family
-
+    
     @property
     def _initialized(self):
         return self.__initialized
-
+    
     @classmethod
     def get_instance(cls, pkcs11_lib_path, hsm_family):
         if pkcs11_lib_path not in cls.__known_lib:
@@ -100,10 +100,10 @@ class PKCS11Lib:
                 if pkcs11_lib_path not in cls.__known_lib:
                     cls.__known_lib[pkcs11_lib_path] = cls(pkcs11_lib_path, hsm_family)
         return cls.__known_lib[pkcs11_lib_path]
-
+    
     def get_token(self, slot):
         return Token(pkcs11lib=self, slot=slot)
-
+    
     def get_slot_list(self, tokenPresent=True):
         with self:
             # Number of slot
@@ -113,7 +113,7 @@ class PKCS11Lib:
                 RV = self.__lib.C_GetSlotList(tokenPresent, None, byref(count))
             if RV != 0:
                 raise Exception(f'C_GetSlotList failed with error {Error(RV).name}')
-
+            
             logger.info(f"Slot Count={count.value}")
             # Slot list buffer
             slot_list = (c_ulong * count.value)()
@@ -146,25 +146,25 @@ class Token:
         self.__slot = slot
         self.__opened_session = None
         self.__session_lock = Lock()
-
+    
     @property
     def pkcs11lib(self):
         return self.__pkcs11lib
-
+    
     @property
     def remote_op_lock(self):
         return self.__pkcs11lib.remote_op_lock
-
+    
     @property
     def slot(self):
         return self.__slot
-
+    
     def session(self, pin=None):
         with self.__session_lock:
             if self.__opened_session is None or self.__opened_session.is_session_terminated():
                 self.__opened_session = Session(token=self, pin=pin)
             return self.__opened_session
-
+    
     def get_mechanism_info(self, mecanism):
         with self.__pkcs11lib:
             assert isinstance(mecanism, Mechanism)
@@ -202,12 +202,12 @@ class Session:
         self.__pin = pin
         self.__session_opened = False
         self.__logged_in = False
-
+    
     def _open(self):
         logger.debug('Entering Session._open')
         # Initialize an empty cache
         self._obj_handle_cache = {}
-
+        
         """Will open a session and then login using the PIN argument"""
         # Open the session
         slotID = self.__token.slot
@@ -220,9 +220,9 @@ class Session:
         if hSession.value <= 0:
             raise Exception(f'C_OpenSession returned a bad handle: {hSession.value}')
         self.__handle = hSession
-
+        
         logger.debug('Exiting Session._open')
-
+    
     def _login(self):
         logger.debug('Entering Session._login')
         user_type = UserType.USER
@@ -234,7 +234,7 @@ class Session:
         if RV != 0:
             raise Exception(f'C_Login failed with error {Error(RV).name}')
         logger.debug('Exiting Session._login')
-
+    
     def _logout(self):
         logger.debug('Entering Session._logout')
         if self.__logged_in:
@@ -242,7 +242,7 @@ class Session:
             if RV != 0:
                 raise Exception(f'C_Logout failed with error {Error(RV).name}')
         logger.debug('Exiting Session._logout')
-
+    
     def _close(self):
         logger.debug('Entering Session._close')
         if self.__session_opened:
@@ -250,7 +250,7 @@ class Session:
             if RV != 0:
                 raise Exception(f'C_CloseSession failed with error {Error(RV).name}')
         logger.debug('Exiting Session._close')
-
+    
     def __enter__(self):
         with self.__open_lock:
             if self.__open_count == 0:
@@ -260,7 +260,7 @@ class Session:
                     self._login()
             self.__open_count += 1
         return self
-
+    
     def __exit__(self, type_, value, traceback):
         with self.__open_lock:
             self.__open_count -= 1
@@ -271,24 +271,24 @@ class Session:
                     self._close()
                 if self.__pkcs11lib._initialized:
                     self.__pkcs11lib._exit()
-
+    
     def is_session_terminated(self):
         with self.__open_lock:
             return self.__open_count <= 0 and self.__session_opened
-
+    
     def get_session_info(self):
         # Session handle
         hSession = self.__handle
-
+        
         # Init a CK_SESSION_INFO structure filled with 0s
         session_info = CK_SESSION_INFO(0, 0, 0, 0)
-
+        
         # Make the call
         with self.__remote_op_lock:
             RV = self.__lib.C_GetSessionInfo(hSession, byref(session_info))
         if RV != 0:
             raise Exception(f'C_GetSessionInfo failed with error {Error(RV).name}')
-
+        
         # Return dict with values
         return {
             'slotID': session_info.slotID,
@@ -296,7 +296,7 @@ class Session:
             'flags': [f.name for f in session_info.get_flags()],
             'ulDeviceError': session_info.ulDeviceError,
         }
-
+    
     def generate_random(self, size):
         """Use the token to generate random bytes of `size` bytes"""
         # Create a buffer of len `size`
@@ -310,7 +310,7 @@ class Session:
         if len(buf) != size:
             raise Exception('The random data size returned by the token did not match the requested size')
         return buf
-
+    
     def get_object_handle(self, label=None):
         if label in self._obj_handle_cache:
             return self._obj_handle_cache[label]
@@ -320,7 +320,7 @@ class Session:
         if label is not None:
             attrs.append(CK_ATTRIBUTE.construct(Attribute.LABEL, label))
         template = CK_ATTRIBUTE.get_array_of(attrs)
-
+        
         with self.__remote_op_lock:
             # Initialize a search
             search_it = SearchIter(lib=self.__lib, hSession=self.__handle, template=template)
@@ -339,7 +339,7 @@ class Session:
                 # That's the expected behavior in our case
                 self._obj_handle_cache[label] = objHandle
                 return objHandle
-
+    
     def create_secret_key(self,
             key_label,
             key_gen_mech=Mechanism.AES_KEY_GEN,
@@ -384,7 +384,7 @@ class Session:
         template = CK_ATTRIBUTE.get_array_of(attrs)
         # Future key handle
         h = c_ulong()
-
+        
         with self.__remote_op_lock:
             RV = self.__lib.C_GenerateKey(hSession, pMechanism, byref(template), len(template), byref(h))
         if RV != 0:
@@ -445,7 +445,7 @@ class Session:
                 CK_PRF_DATA_PARAM.construct(PrfDataType.SP800_108_PRF_CONTEXT, nonce)
             ]
             kdf_data_params = CK_PRF_DATA_PARAM.get_array_of(kdf_data_params)
-
+            
             mech_param = CK_SP800_108_KDF_PARAMS(
                 Mechanism.SHA256_HMAC,
                 len(kdf_data_params),
@@ -455,7 +455,7 @@ class Session:
             pMechanism = byref(CK_MECHANISM.construct(derive_key_mech, mech_param))
         else:
             raise Exception(f'Mechanism {derive_key_mech} is not supported')
-
+        
         # Template
         attrs = [
             CK_ATTRIBUTE.construct(Attribute.LABEL, key_label),
@@ -479,7 +479,7 @@ class Session:
         template = CK_ATTRIBUTE.get_array_of(attrs)
         # Future key handle
         h = c_ulong()
-
+        
         with self.__remote_op_lock:
             RV = self.__lib.C_DeriveKey(hSession, pMechanism, c_ulong(base_key_handle), byref(template), len(template), byref(h))
         if RV != 0:
@@ -513,12 +513,12 @@ class Session:
             raise Exception(f'Mechanism {wrap_key_mech} is not supported')
         # Buffer
         buf, buf_len = get_buffer()
-
+        
         with self.__remote_op_lock:
             RV = self.__lib.C_WrapKey(hSession, pMechanism, c_ulong(wrap_key_handle), c_ulong(key_handle), buf, byref(buf_len))
         if RV != 0:
             raise Exception(f'C_WrapKey failed with error {Error(RV).name}')
-
+        
         # Result size
         wrap_size = buf_len.value
         return buf[:wrap_size]
@@ -547,7 +547,7 @@ class Session:
                 if wrap_key_iv is None:
                     wrap_key_iv = generate_random(12) # Generate a 96-bits random value
                 piv, iv_len = get_void_pointer_and_len(wrap_key_iv)
-
+            
             paad, aad_len = get_void_pointer_and_len(aad)
             mech_param = CK_GCM_PARAMS(
                 piv,
@@ -563,17 +563,17 @@ class Session:
             raise Exception(f'Mechanism {wrap_key_mech} is not supported')
         # Buffer
         buf, buf_len = get_buffer()
-
+        
         with self.__remote_op_lock:
             RV = self.__lib.C_WrapKey(hSession, pMechanism, c_ulong(wrap_key_handle), c_ulong(key_handle), buf, byref(buf_len))
         if RV != 0:
             raise Exception(f'C_WrapKey failed with error {Error(RV).name}')
-
+        
         # Result size
         wrap_size = buf_len.value
         # We return the IV and the result
         return get_value_from_void_pointer_and_len(bytes, piv, iv_len), buf[:wrap_size]
-
+    
     def unwrap_key_sym(self,
             key_data,
             wrap_key_handle,
@@ -648,7 +648,7 @@ class Session:
         template = CK_ATTRIBUTE.get_array_of(attrs)
         # Future key handle
         h = c_ulong()
-
+        
         with self.__remote_op_lock:
             RV = self.__lib.C_UnwrapKey(hSession, pMechanism, c_ulong(wrap_key_handle), pwk, pwk_len, byref(template), len(template), byref(h))
         if RV != 0:
@@ -656,7 +656,7 @@ class Session:
         if h.value <= 0:
             raise Exception(f'C_UnwrapKey returned a bad handle: {h.value}')
         return h.value
-
+    
     def import_rsa_public_key(self,
             key_label,
             modulus,
@@ -689,7 +689,7 @@ class Session:
         template = CK_ATTRIBUTE.get_array_of(attrs)
         # Future key handle
         h = c_ulong()
-
+        
         with self.__remote_op_lock:
             RV = self.__lib.C_CreateObject(hSession, byref(template), len(template), byref(h))
         if RV != 0:
@@ -697,7 +697,7 @@ class Session:
         if h.value <= 0:
             raise Exception(f'C_CreateObject returned a bad handle: {h.value}')
         return h.value
-
+    
     def create_ec_key_pair(self,
             private_key_label,
             public_key_label,
@@ -747,7 +747,7 @@ class Session:
         # Future key handles
         h_pub = c_ulong()
         h_priv = c_ulong()
-
+        
         with self.__remote_op_lock:
             RV = self.__lib.C_GenerateKeyPair(hSession, pMechanism, byref(template_pub), len(template_pub), byref(template_priv), len(template_priv), byref(h_pub), byref(h_priv))
         if RV != 0:
@@ -755,7 +755,7 @@ class Session:
         if h_pub.value <= 0 or h_priv.value <= 0:
             raise Exception(f'C_GenerateKeyPair returned a bad handle: ({h_pub.value},{h_priv.value})')
         return h_pub.value, h_priv.value
-
+    
     def sign(self,
         key_handle,
         signing_mech,
@@ -763,16 +763,16 @@ class Session:
         ):
         # Session handle
         hSession = self.__handle
-
+        
         # Mechanism
         pMechanism = byref(CK_MECHANISM.construct(signing_mech))
-
+        
         # Get pointer
         pdata, data_len = get_void_pointer_and_len(data)
-
+        
         # Buffer
         buf, buf_len = get_buffer()
-
+        
         # Sign
         with self.__remote_op_lock:
             # Init signature
@@ -783,69 +783,69 @@ class Session:
             RV = self.__lib.C_Sign(hSession, pdata, data_len, buf, byref(buf_len))
             if RV != 0:
                 raise Exception(f'C_Sign failed with error {Error(RV).name}')
-
+        
         # Result size
         sig_size = buf_len.value
         return buf[:sig_size]
-
+    
     def get_attributes(self,
         object_handle,
         attr_list
         ):
         # Session handle
         hSession = self.__handle
-
+        
         # Template
         attrs = [
             CK_ATTRIBUTE.construct(Attribute[a.upper()], get_buffer()[0])
             for a in attr_list
         ]
         template = CK_ATTRIBUTE.get_array_of(attrs)
-
+        
         with self.__remote_op_lock:
             RV = self.__lib.C_GetAttributeValue(hSession, c_ulong(object_handle), byref(template), len(template))
         if RV != 0:
             raise Exception(f'C_GetAttributeValue failed with error {Error(RV).name}')
-
+        
         # Convert the template back to a dict of key:val with the attributes name passed to the method
         template_index = {
             Attribute(attr_struct.type).name:attr_struct.get_value()
             for attr_struct in template
         }
-
+        
         return {
             a:template_index[a.upper()]
             for a in attr_list
         }
-
+    
     def destroy_object(self,
         object_handle
         ):
         # Session handle
         hSession = self.__handle
-
+        
         with self.__remote_op_lock:
             RV = self.__lib.C_DestroyObject(hSession, c_ulong(object_handle))
         if RV != 0:
             raise Exception(f'C_DestroyObject failed with error {Error(RV).name}')
-
+        
         return True
 
 class SearchIter:
     """Iterate a search for objects on a session."""
-
+    
     def __init__(self, lib, hSession, template):
         self.__active = True
         self.__lib = lib
         self.__handle = hSession
-
+        
         RV = self.__lib.C_FindObjectsInit(hSession, byref(template), len(template))
         if RV != 0:
             raise Exception(f'C_FindObjectsInit failed with error {Error(RV).name}')
-
+    
     def __iter__(self):
         return self
-
+    
     def __next__(self):
         """Get the next object."""
         h = c_ulong()
@@ -853,21 +853,21 @@ class SearchIter:
         RV = self.__lib.C_FindObjects(self.__handle, byref(h), 1, byref(count))
         if RV != 0:
             raise Exception(f'C_FindObjects failed with error {Error(RV).name}')
-
+        
         if count.value == 0:
             self._finalize()
             raise StopIteration()
         else:
             return h.value
-
+    
     def __del__(self):
         """Close the search."""
         self._finalize()
-
+    
     def close(self):
         """Close the search."""
         self._finalize()
-
+    
     def _finalize(self):
         """Finish the operation."""
         if self.__active:
@@ -880,33 +880,33 @@ class SearchIter:
 # TODO: Correctly implement locking mechanism
 class SignObject:
     """Allow to perform a signature"""
-
+    
     def __init__(self, lib, hSession, key_handle, signing_mech):
         self.__active = True
         self.__lib = lib
         self.__handle = hSession
-
+        
         # Mechanism
         pMechanism = byref(CK_MECHANISM.construct(signing_mech))
         RV = self.__lib.C_SignInit(self.__handle, pMechanism, c_ulong(key_handle))
         if RV != 0:
             raise Exception(f'C_SignInit failed with error {Error(RV).name}')
-
+    
     def __del__(self):
         """Close the sign operation."""
         if self.__active:
             self.signature()
-
+    
     def update(self, data):
         pPart, part_len = get_void_pointer_and_len(data)
         RV = self.__lib.C_SignUpdate(self.__handle, pPart, part_len)
         if RV != 0:
             raise Exception(f'C_SignUpdate failed with error {Error(RV).name}')
-
+    
     def signature(self):
         # Buffer
         buf, buf_len = get_buffer()
-
+        
         RV = self.__lib.C_SignFinal(self.__handle, buf, byref(buf_len))
         if RV != 0:
             raise Exception(f'C_SignFinal failed with error {Error(RV).name}')
